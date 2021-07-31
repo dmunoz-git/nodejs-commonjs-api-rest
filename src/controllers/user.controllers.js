7350954f41cab9c3814b07aa64d83e801f5608a1
dmunoz-git <dmunozGit@gmail.com>
2021-07-31 23:21:02 +0200

    Complete user controller CRUD

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,5 +1,14 @@
 const User = require("../models/user.model");
 
+const createUser = (req, res) => {
+    User.findOne({ email: req.body.mail }).then((user) => {
+        if (user) {
+            const user = new User(req.body);
+            res.status.status(201).send(user);
+        }
+    });
+};
+
 const updateUser = (req, res) => {
     User.updateOne({ email: req.body.email })
         .then((user) => {
@@ -14,6 +23,16 @@ const deleteUser = (req, res) => {
             res.status(201).send(user);
         })
         .catch(() => res.status(500).send({ message: "Internal server error" }));
+};
+
+const getUsers = (req, res) => {
+    User.find({}).then((users) => {
+        if (users.lenght > 0) {
+            res.status(201).send(users);
+        } else {
+            res.status(404).send({ message: "there is not users" });
+        }
+    });
 };
 
 module.exports = {
--- src/middlewares/user.middleware.js
+++ src/middlewares/user.middleware.js
@@ -1,23 +1,3 @@
 const User = require("../models/user.model");
 
-const validateUserPassword = (req, res, next) => {
-    User.findOne({ email: req.body.email } || { user: req.body.user })
-        .then((user) => {
-            if (user) {
-                User.comparePassword(req.body.password, user.password).then((match) => {
-                    console.log(match);
-                    if (match) {
-                        req.user = user;
-                        next();
-                    } else {
-                        res.status(401).send({ message: "User not authorizated" });
-                    }
-                });
-            } else {
-                res.status(404).send({ message: "User not found" });
-            }
-        })
-        .catch(() => res.status(500).send({ message: "Internal server error" }));
-};
-
 module.exports = { validateUserPassword };
