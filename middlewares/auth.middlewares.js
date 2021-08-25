0238015a1bf28f0a6aa17a83c34c420668ec96c4
dmunoz-git <dmunozGit@gmail.com>
2021-08-25 10:53:26 +0200

    Modify refresh controller

--- middlewares/auth.middlewares.js
+++ middlewares/auth.middlewares.js
@@ -1,6 +1,5 @@
 const jwt = require("jsonwebtoken");
 const config = require("../config");
-const User = require("../models/user.model");
 
 const verifyAuth = (req, res, next) => {
     const token = req.headers.authorization;
