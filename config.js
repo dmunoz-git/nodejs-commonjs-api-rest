567bed0b4271f4146292b41b41de2e85fcbe6366
dmunoz-git <dmunozGit@gmail.com>
2023-12-20 22:35:07 +0100

    fix wrong db address

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     nodePort: process.env.PORT || 3000,
-    mongoURL: process.env.MONGODB_TEST || process.env.MONGODB_DEV || "mongodb://localhost:27017/api-books",
+    mongoURL: process.env.MONGODB_TEST || process.env.MONGODB_DEV || "mongodb://mongodb:27017/api-books",
     auth: {
         accessTokenKey: process.env.ACCESS_TOKEN_KEY || "secret",
         refreshTokenKey: process.env.REFRESH_TOKEN_KEY || "secret2",
